5a9aeb2e5e108967bc67c5699c7b0766d98713c7
Karan <karandeep123@gmail.com>
2023-04-08 02:34:57 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Home from "./Home";
+import Header from "./Header";
 function App() {
   return (
-    <div className="App">
-      <div className="first">this is my first ecommerce application</div>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
 export default App;
